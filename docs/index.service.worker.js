f1b5e3432082924e686f42b6b8c362718c5edf9a
Worm v <willarddh1996@gmail.com>
2025-04-02 14:15:51 -0400

    Updated UI for better usability.

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743601933|100796632';
+const CACHE_VERSION = '1743617637|5327901997';
 /** @type {string} */
 const CACHE_PREFIX = 'Input Lag Reduce-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
